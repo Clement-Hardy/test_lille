e2f3dc52f1fc18014708684afb7b741f8364c528
clemHard <vicclem5999@live.fr>
2020-02-28 18:00:38 +0100

    change xgboost

--- model/xgboost.R
+++ model/xgboost.R
@@ -1,12 +1,19 @@
-library(xgboost)
 
 
-xgboost_fit_predict <- function(train, test){
-  X_train <- xgb.DMatrix(as.matrix(train %>% select(-y)), label=log(train$y))
-  X_test <- xgb.DMatrix(as.matrix(test %>% select(-y)))
-  xgb <- xgboost(data = X_train,
-                 nrounds = 300)
-  pred <- c(predict(xgb, newdata=X_train), predict(xgb, newdata=X_test))
+xgboost_fit_predict <- function(train, test, only_future=TRUE){
+  train <- train %>% subset(select=-Date)
+  test <- test %>% subset(select=-Date)
+  X_train <- xgboost::xgb.DMatrix(as.matrix(train %>% select(-y)), label=log(train$y))
+  X_test <- xgboost::xgb.DMatrix(as.matrix(test %>% select(-y)))
+  model <- xgboost::xgboost(data = X_train,
+                            nrounds = 400,
+                            verbose=0)
+  if (only_future){
+    pred <- predict(model, newdata=X_test)
+  }
+  else{
+    pred <- c(predict(model, newdata=X_train), predict(model, newdata=X_test))
+  }
   pred <- data.frame(ds=1:length(pred)[1], yhat=exp(pred))
   return(pred)
 }
