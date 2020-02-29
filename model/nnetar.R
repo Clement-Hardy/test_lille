056bccc5b8f9b509096b78707666bc3f0a6d58bf
Clément <Hardy>
2020-02-29 13:19:33 +0100

    add nnetar and mlp(not working)

---
+++ model/nnetar.R
@@ -0,0 +1,18 @@
+library(caret)
+
+
+
+nnetar_fit_predict <- function(train, period=12, only_future=TRUE){
+  model <- nnetar(train$y,
+                  decay = 0,
+                  maxit = 50000)
+  pred <- forecast(model, h=period) 
+  fitted_value <- pred$fitted %>% as.numeric()
+  pred <- pred$mean %>% as.numeric()
+  pred <- c(fitted_value, pred)
+  pred <- data.frame(ds=1:length(pred)[1], yhat=pred)
+  if (only_future){
+    pred <- pred%>%top_n(period, wt=ds)
+  }
+  return(pred)
+}
